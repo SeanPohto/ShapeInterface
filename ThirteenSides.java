234708a3408a0824d741a834955b8f2a4dee668d
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-12-15 11:37:36 -0800

    12/15

---
+++ ThirteenSides.java
@@ -0,0 +1,52 @@
+
+/**
+ * 
+ * 
+ * @author  
+ * @version
+ */
+public class ThirteenSides implements Shape
+{
+  private int sideLength;
+  private String color;
+
+  /**
+   * Constructor for objects of class Circle
+   */
+  public ThirteenSides(int radius, String color)
+  {
+    this.sideLength = sideLength;
+    this.color = color;
+  }
+
+  /**
+   * Get the area
+   * 
+   * @return     returns the area of the circle
+   */
+  public double getArea()
+  {
+    this.sideLength
+    return (13/2) * this.sideLength * (
+  }
+
+  /**
+   * Get the perimeter
+   * 
+   * @return     returns the perimeter of the circle
+   */
+  public double getPerimeter()
+  {
+    return 13 * this.sideLength;
+  }
+
+  /**
+   * Get the color
+   * 
+   * @return     returns the color of the circle
+   */
+  public String getColor()
+  {
+    return this.color;
+  }
+}
